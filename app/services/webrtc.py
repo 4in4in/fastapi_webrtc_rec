dde6992e860e18d9c3ea1b8bdcb03d33a077119c
alex <alex.thunder@tut.by>
2023-03-31 20:29:18 +0300

    refactor: decompose add_peer_connection feat: add .gitignore

--- app/services/webrtc.py
+++ app/services/webrtc.py
@@ -24,63 +24,75 @@ class WebRTCService:
         self._relay = MediaRelay()  # or create for each client individually?
         self._rec_path = rec_path
 
+    def _add_on_datachannel(self, connection: RTCPeerConnection, peer_id: UUID) -> None:
+        @connection.on("datachannel")
+        def on_datachannel(channel: RTCDataChannel):
+            @channel.on("message")
+            def on_message(message: str):
+                self._logger.info("MESSAGE %s: %s", peer_id, message)
+
+    def _add_on_connection_state_change(
+        self, connection: RTCPeerConnection, peer_id: UUID
+    ) -> None:
+        @connection.on("connectionstatechange")
+        async def on_connection_state_change():
+            self._logger.info("Connection state is %s", connection.connectionState)
+            if connection.connectionState == "failed":
+                await connection.close()
+                del self._peer_connections[peer_id]
+
+    def _add_on_track(
+        self,
+        connection: RTCPeerConnection,
+        peer_id: UUID,
+        recorder: MediaRecorder,
+        transform: str,
+    ) -> None:
+        @connection.on("track")
+        def on_track(track: MediaStreamTrack):
+            self._logger.info("%s: Track %s received", peer_id, track.kind)
+
+            if track.kind == "audio":
+                recorder.addTrack(track)
+            elif track.kind == "video":
+                _track = self._relay.subscribe(track)
+                _transform_track = VideoTransformTrack(_track, transform)
+                connection.addTrack(_transform_track)
+
+                recorder.addTrack(self._relay.subscribe(track))
+
+            @track.on("ended")
+            async def on_ended():
+                self._logger.info("%s: Track %s ended", peer_id, track.kind)
+                await recorder.stop()
+
     async def add_peer_connection(
         self, peer_id: UUID, connection: RTCPeerConnection, params: OfferSchema
     ) -> OfferSchema:
-        if peer_id not in self._peer_connections:
-            offer = RTCSessionDescription(params.sdp, params.type)
-            self._peer_connections[peer_id] = connection
-            self._logger.info("peer %s is connected", peer_id)
-
-            recorder = MediaRecorder(f"{self._rec_path}/{str(peer_id)}.mp4")
-
-            @connection.on("datachannel")
-            def on_datachannel(channel: RTCDataChannel):
-                @channel.on("message")
-                def on_message(message: str):
-                    self._logger.info("MESSAGE %s: %s", peer_id, message)
-
-            @connection.on("connectionstatechange")
-            async def on_connection_state_change():
-                self._logger.info("Connection state is %s", connection.connectionState)
-                if connection.connectionState == "failed":
-                    await connection.close()
-                    del self._peer_connections[peer_id]
-
-            @connection.on("track")
-            def on_track(track: MediaStreamTrack):
-                self._logger.info("%s: Track %s received", peer_id, track.kind)
-
-                if track.kind == "audio":
-                    recorder.addTrack(track)
-                elif track.kind == "video":
-                    _track = self._relay.subscribe(track)
-                    _transform_track = VideoTransformTrack(
-                        _track, params.video_transform
-                    )
-                    connection.addTrack(_transform_track)
-
-                    recorder.addTrack(self._relay.subscribe(track))
-
-                @track.on("ended")
-                async def on_ended():
-                    self._logger.info("%s: Track %s ended", peer_id, track.kind)
-                    await recorder.stop()
-
-            await connection.setRemoteDescription(offer)
-            await recorder.start()
-
-            answer = await connection.createAnswer()
-            await connection.setLocalDescription(answer)
-
-            return OfferSchema(
-                sdp=connection.localDescription.sdp,
-                type=connection.localDescription.type,
-            )
-
-        else:
+        if peer_id in self._peer_connections:
             self._logger.error("peer %s is connected already!", peer_id)
             raise Exception
+
+        offer = RTCSessionDescription(params.sdp, params.type)
+        self._peer_connections[peer_id] = connection
+        self._logger.info("peer %s is connected", peer_id)
+
+        recorder = MediaRecorder(f"{self._rec_path}/{str(peer_id)}.mp4")
+
+        self._add_on_datachannel(connection, peer_id)
+        self._add_on_connection_state_change(connection, peer_id)
+        self._add_on_track(connection, peer_id, recorder, params.video_transform)
+
+        await connection.setRemoteDescription(offer)
+        await recorder.start()
+
+        answer = await connection.createAnswer()
+        await connection.setLocalDescription(answer)
+
+        return OfferSchema(
+            sdp=connection.localDescription.sdp,
+            type=connection.localDescription.type,
+        )
 
     async def close_all_connections(self):
         self._logger.info("closing all connections...")
